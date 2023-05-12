65b4a7b04b5e0a4bff5e4145ee98e56f9621aaef
Sijie <devhacker520@hotmail.com>
2023-05-12 23:47:03 +0800

    增加 point 拖拽、size、方向

--- assets/modules/player/point/point.ts
+++ assets/modules/player/point/point.ts
@@ -1,14 +1,97 @@
-import { _decorator, Component, Node } from 'cc';
+/**
+ * 控制点
+ * 包含方向、圆大小、位置
+ */
+
+import {
+    _decorator,
+    ccenum,
+    
+    Component,
+    UITransform,
+
+    Vec2,
+    Vec3,
+    Size,
+
+    EventTouch,
+    NodeEventType,
+} from 'cc';
 const { ccclass, property } = _decorator;
+
+enum PointDirectionEnum {
+    UP = 0,
+    RIGHT = 1,
+    DOWN = 2,
+    LEFT = 3,
+};
+ccenum(PointDirectionEnum);
 
 @ccclass('point')
 export class point extends Component {
-    start() {
 
+    /**
+     * 控制点方向
+     */
+    @property({ type: PointDirectionEnum })
+    get direction() : PointDirectionEnum {
+        return this._direction;
+    }
+    set direction(val: PointDirectionEnum) {
+        this._direction = val;
+        this.node.setRotationFromEuler(0, 0, this._direction * 90);
+    }
+    _direction: PointDirectionEnum = PointDirectionEnum.UP;
+
+    /**
+     * 控制点大小
+     */
+    @property()
+    get size() {
+        return this._size;
+    }
+    set size(val: number) {
+        this._updateSize();
+        this._size = val;
+    }
+    _size: number = 100;
+
+    _offset: Vec2 = new Vec2(960 / 2, 640 / 2);
+
+    onLoad() {
+        this._updateEuler();
+        this._updateSize();
+    }
+
+    start() {}
+
+    onEnable() {
+        this.node.on(NodeEventType.TOUCH_MOVE, this._onTouchMove, this);
+    }
+
+    onDisable() {
+        this.node.off(NodeEventType.TOUCH_MOVE, this._onTouchMove, this);
+    }
+
+    _updateSize() {
+        const transform = this.node.getComponent('cc.UITransform') as UITransform;
+        transform.contentSize = new Size(this._size, this._size);
+    }
+
+    _updateEuler() {
+        this.node.setRotationFromEuler(0, 0, this.direction * 90);
+    }
+
+    private _onTouchMove(event: EventTouch) {
+        const uiPos = event.getUILocation();
+        uiPos.x -= this._offset.x;
+        uiPos.y -= this._offset.y;
+        this.node.setPosition(new Vec3(uiPos.x, uiPos.y));
     }
 
     update(deltaTime: number) {
         
     }
+
 }
 
