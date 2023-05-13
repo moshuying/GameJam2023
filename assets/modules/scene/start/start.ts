6febf0d2ee460624468f043277edaf4f094372b7
Sijie <devhacker520@hotmail.com>
2023-05-13 09:30:00 +0800

    场景切换功能

--- assets/modules/scene/start/start.ts
+++ assets/modules/scene/start/start.ts
@@ -1,8 +1,26 @@
-import { _decorator, Component, Node } from 'cc';
+import {
+    _decorator,
+    Component,
+    Node,
+    Prefab,
+    instantiate,
+} from 'cc';
+
 const { ccclass, property } = _decorator;
 
 @ccclass('start')
 export class start extends Component {
+
+    @property({
+        type: [Prefab]
+    })
+    sceneList: Prefab[] = [];
+
+    @property({
+        type: [Node],
+    })
+    startNodeList: Node[] = [];
+
     start() {
 
     }
@@ -10,5 +28,28 @@ export class start extends Component {
     update(deltaTime: number) {
         
     }
-}
 
+    // 
+
+    currentSceneIndex: number = 0;
+    currentSceneNode: Node | null = null;
+
+    nextScene() {
+        if (this.currentSceneNode) {
+            const parent = this.currentSceneNode.getParent();
+            parent.removeChild(this.currentSceneNode);
+        }
+
+        const prefab = this.sceneList[this.currentSceneIndex];
+        this.currentSceneNode = instantiate(prefab.data);
+        this.node.addChild(this.currentSceneNode);
+        this.currentSceneIndex++;
+
+        // start 场景才显示
+        this.startNodeList.forEach(node => node.active = this.currentSceneIndex === 0);
+    }
+
+    prefabScene() {
+
+    }
+}
